6fab90c74c71da4445c8bbc019c77569f1eceb4b
Elliot Moral <morales1996an@gmail.com>
2024-05-30 15:11:55 -0500

    mejora en ver el nombre de la ciudad en otros hoteles cerca

--- src/components/HotelIdPage/OthersHotels.jsx
+++ src/components/HotelIdPage/OthersHotels.jsx
@@ -4,23 +4,22 @@ import HotelCard from "../HomePage/HotelCard";
 import './styles/OthersHotels.css'
 
 const OthersHotels = ({city, id}) => {
-
+    
     const [ hotelByCity, getHotelByCity ] = useFetch();
-  
-    console.log(hotelByCity)
-  
+    
+    
     useEffect(()=>{
-
+        
         if(city){
             const url = `https://hotels-api.academlo.tech/hotels?cityId=${city?.id}`;
             getHotelByCity(url);
         }
-
+        
     }, [city])
-
+    
     return (
         <section className="others__hotles_content">
-            <h3>Others Hotels in <span>{city?.country}</span></h3>
+            <h3>Others Hotels in <span>{city?.name}</span></h3>
             <div className="conainter__othersHotels">
                 {   //# hago el esfuerzo e enteder la linea siguiente del filter.
                     hotelByCity?.filter(hotel => hotel.id !== id).map(hotel => (
